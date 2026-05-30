e8c15889e5729b3bb386afd4729cdf78f9a37e0c
Kobe Smits <12503548@student.pxl.be>
2026-05-30 12:26:04 +0200

    WPL 2

---
+++ script.js
@@ -0,0 +1,62 @@
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["IT-Student aan PXL", "Junior Developer", "Probleemoplosser", "Toekomstig Programmeur"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }
+    else {
+        cursorSpan.classList.remove("typing");
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    }
+    else {
+        cursorSpan.classList.remove("typing");
+        textArrayIndex++;
+        if(textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.15 // Start de animatie als 15% van het blok in beeld is
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+            observer.unobserve(entry.target);
+        }
+    });
+}, observerOptions);
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('.reveal').forEach((element) => {
+        observer.observe(element);
+    });
+});
